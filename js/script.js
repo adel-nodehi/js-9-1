0359da4b220e8837f2d9f26be26959b824af4349
Adel <adelnmo2003@gmail.com>
2024-11-16 23:01:37 +0330

    async-await and try-catch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 "use strict";
+
+const btnGetAdvice = document.querySelector(".get-data");
+
 /*
 setTimeout(function () {
   console.log("1 second passed");
@@ -16,12 +19,34 @@ setTimeout(function () {
 
 // creating Promise
 
+const createCard = function ({ slip: adviceData }) {
+  const div = document.createElement("div");
+
+  const id = document.createElement("p");
+  id.textContent = adviceData.id;
+
+  const advice = document.createElement("p");
+  advice.textContent = adviceData.advice;
+
+  div.append(id, advice);
+
+  document.querySelector(".container").append(div);
+};
+
+const showError = function (errorMessage) {
+  const p = document.createElement("p");
+  p.textContent = `💣💣 Something went wrong: ${errorMessage}`;
+
+  document.body.prepend(p);
+};
+
 const wait = function (second = 1) {
   return new Promise((resolve) => {
     setTimeout(resolve, second * 1000);
   });
 };
 
+/*
 wait()
   .then(function () {
     console.log("1 second passed");
@@ -41,3 +66,39 @@ wait()
   .then(function () {
     console.log("4 second passed");
   });
+*/
+
+// fetch("https://api.adviceslip.com/advice")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+// https://api.adviceslip.com/advice/{slip_id}
+
+const getAdvice = async function (adviceId) {
+  try {
+    const response = await fetch(
+      `https://api.adviceslip.com/advice/${adviceId}`
+    );
+
+    const data = await response.json();
+
+    if (data.message?.type === "error") {
+      throw new Error(`invalid id: ${adviceId}`);
+    }
+
+    createCard(data);
+  } catch (err) {
+    // console.error(`💣💣💣 Something goes wrong: ${err.message}`);
+    showError(err.message);
+  }
+};
+
+getAdvice(18000);
+
+getAdvice(15);
+
+// btnGetAdvice.addEventListener("click", getAdvice);
