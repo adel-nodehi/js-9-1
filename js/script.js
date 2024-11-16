a1b3c6312bb7df21af1a179484e667698e3a2a4e
Adel <adelnmo2003@gmail.com>
2024-11-16 20:33:36 +0330

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+setTimeout(function () {
+  console.log("1 second passed");
+  setTimeout(function () {
+    console.log("2 second passed");
+    setTimeout(function () {
+      console.log("3 second passed");
+      setTimeout(function () {
+        console.log("4 second passed");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+*/
+
+// creating Promise
+
+new Promise((resolve, reject) => {
+  setTimeout(function () {
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("1 second passed");
+});
